6ecab5c0d00fa81a206aacd2f6251874322496a9
Shubham Patrick <ahthneeuhl@gmail.com>
2023-05-31 17:52:39 +0530

    config: configure tailwindCSS & screens

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import { HomeScreen, ProductScreen } from "screens";
+import { applicationRoutes as routes } from "config";
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path={routes.home} element={<HomeScreen />} />
+      <Route path={routes.product} element={<ProductScreen />} />
+    </Routes>
   );
-}
+};
 
 export default App;
---
+++ src/config/applicationRoutes.ts
@@ -0,0 +1,4 @@
+const home = "/";
+const product = "/product";
+
+export { home, product };
